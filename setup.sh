bd9cd26f9af19ddd45377c1e788ff188b0467495
shruti-garg30 <shrutigarg1999@gmail.com>
2023-05-08 15:57:27 -0400

    Adding setup file

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+pip install amadeus
+pip install flask_sqlalchemy
+pip install flask_cors
+pip install pyspark
+pip install psycopg2
+pip install flask_pymongo
+npm install 
+psql -U postgres
+CREATE DATABASE cheapThrills
+exit
+python /flask-server/database.py
+exec exit
+exec $SHELL
+
