0f0bb6ccc8f1f2826b4920d166ae243a14a8fcca
shruti-garg30 <shrutigarg1999@gmail.com>
2023-05-11 19:51:01 -0400

    Updating Setup File

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+pip install flask
 pip install amadeus
 pip install flask_sqlalchemy
 pip install flask_cors
@@ -6,9 +7,9 @@ pip install pyspark
 pip install psycopg2
 pip install flask_pymongo
 npm install 
-psql -U postgres
-CREATE DATABASE cheapThrills
-exit
+docker run --name postgres -p 5432:5432 --restart always -e POSTGRES_PASSWORD=123 -d postgres
+docker exec -it postgres psql -U postgres -c "CREATE DATABASE cheapThrills;"
+docker pull mongo
 python /flask-server/database.py
 exec exit
 exec $SHELL
