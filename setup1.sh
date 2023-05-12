00abc8666cef73677fc5bcc41a46d629cebec38c
Purvav Punyani <50676996+Purvav0511@users.noreply.github.com>
2023-05-11 23:46:30 -0400

    Update and rename setup.sh to setup1.sh

--- setup1.sh
+++ setup1.sh
@@ -6,11 +6,13 @@ pip install flask_cors
 pip install pyspark
 pip install psycopg2
 pip install flask_pymongo
-pip install pandas
 cd client
 npm install 
 cd ..
+docker run --name postgres -p 5432:5432 -e POSTGRES_PASSWORD=123 -d postgres
+docker exec -u postgres postgres createdb cheapThrills
+docker pull mongo
+docker run --name mongo -p 27017:27017 -d mongo
 python flask-server/database.py
 exec exit
 exec $SHELL
-
