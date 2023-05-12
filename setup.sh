dab0cab6abda081ef39840c3808ef26f215fa8fd
sheenagarg9 <sheenagarg.9@gmail.com>
2023-05-11 23:23:10 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,12 +6,10 @@ pip install flask_cors
 pip install pyspark
 pip install psycopg2
 pip install flask_pymongo
+cd client
 npm install 
-docker run --name postgres -p 5432:5432 --restart always -e POSTGRES_PASSWORD=123 -d postgres
-docker exec -it postgres psql -U postgres -c "CREATE DATABASE cheapThrills;"
-docker run --name mongo -e MONGO_INITDB_ROOT_USERNAME=mongo -e MONGO_INITDB_ROOT_PASSWORD=123 -p 27017:27017 -d mongo
-docker exec -it mongo mongo admin --eval "db.createUser({user: 'mongo', pwd: '123', roles: [{role: 'root', db: 'admin'}]}); db.createDatabase('cheapThrills');"
-python /flask-server/database.py
+cd ..
+python flask-server/database.py
 exec exit
 exec $SHELL
 
