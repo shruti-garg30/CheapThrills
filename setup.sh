82deeca7f1fe7db0f6e1a44390f614b15d6a6465
shruti-garg30 <shrutigarg1999@gmail.com>
2023-05-11 20:39:21 -0400

    Mongo Changes

--- setup.sh
+++ setup.sh
@@ -9,7 +9,8 @@ pip install flask_pymongo
 npm install 
 docker run --name postgres -p 5432:5432 --restart always -e POSTGRES_PASSWORD=123 -d postgres
 docker exec -it postgres psql -U postgres -c "CREATE DATABASE cheapThrills;"
-docker pull mongo
+docker run --name mongo -e MONGO_INITDB_ROOT_USERNAME=mongo -e MONGO_INITDB_ROOT_PASSWORD=123 -p 27017:27017 -d mongo
+docker exec -it mongo mongo admin --eval "db.createUser({user: 'mongo', pwd: '123', roles: [{role: 'root', db: 'admin'}]}); db.createDatabase('cheapThrills');"
 python /flask-server/database.py
 exec exit
 exec $SHELL
